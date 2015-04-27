76c3f890f7e78ebf541c9e28ed2fb213dcc72408
刘鑫 <liuxin7410@qq.com>
2015-04-27 23:52:29 +0800

    4月27日提交。还存在secret option required for sessions的bug,这导致工程不能启动。

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
 var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 
 var app = express();
@@ -7,23 +11,17 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(session());
 
-app.get('/', function(req, res){
-    res.render('index');
-});
+app.listen(app.get('port'));
 
-app.use(function(req, res){
-    res.status(404);
-    res.render('404');
-});
+if('development' === app.get('env')){
+    app.set('showStackError', true);
+    app.use(morgan(':method :url :status'));
+    app.locals.pretty = true;
+}
 
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
-
-app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:' +
-    app.get('port') + ';press Ctrl- C to terminate.');
-});
+require('./config/routes')(app);
+console.log('Project started on port' + app.get('port'));
---
+++ config/routes.js
@@ -0,0 +1,19 @@
+var Index = require('../controllers/index');
+var User = require('../controllers/user');
+
+module.exports = function(app){
+    app.get('/', Index.index);
+
+    app.use(function(req, res){
+        res.status(404);
+        res.render('404');
+    });
+
+    app.use(function(err, req, res, next){
+        console.error(err.stack);
+        res.status(500);
+        res.render('500');
+    });
+
+
+};
